bdd8b0325fc1d188a1cd87e3c7b835b76927652c
jensen <jensen.wang@perkinelmer.com>
2018-07-26 23:05:08 +0800

    initial commit

---
+++ FormAuthentication/Login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FormAuthentication
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                HttpCookie cookie = System.Web.Security.FormsAuthentication.GetAuthCookie(Request.Form["name"], false);
+                
+                Response.Cookies.Add(cookie);
+                var targetUrl = Request.QueryString["ReturnUrl"];
+                if (string.IsNullOrEmpty(targetUrl))
+                {
+                    targetUrl = "Default.aspx";
+                }
+
+                Response.Redirect(targetUrl);
+            }
+        }
+    }
+}
